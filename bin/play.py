3cd2544869814cbfd835523429d023b0fa86d094
Florent Thiéry <florent.thiery@ubicast.eu>
2021-03-30 12:32:15 +0200

    player: add support for downloading videos

--- bin/play.py
+++ bin/play.py
@@ -3,16 +3,22 @@ import json
 import argparse
 import os
 import sys
+from pathlib import Path
+import requests
 
 
 def get_video_urls(presentation):
-    urls = list()
+    videos = dict()
+    slides_stream_type = presentation.get('slides', {}).get('stream_type')
     for video in presentation['videos']:
+        name = video['stream_type']
+        if name == slides_stream_type:
+            name = 'Slides'
         for f in video['files']:
             if f['size_bytes'] > 0 and f['format'] == 'video/mp4':
-                urls.append(f['url'])
+                videos[name] = f['url']
                 break
-    return urls
+    return videos
 
 
 if __name__ == '__main__':
@@ -23,6 +29,20 @@ if __name__ == '__main__':
         type=str,
         help='MediaSite presentation id (space separated)',
         nargs='*',
+    )
+
+    parser.add_argument(
+        '--download',
+        action='store_true',
+        default=False,
+        help='Download to folder instead of playing',
+    )
+
+    parser.add_argument(
+        '--download-folder',
+        type=str,
+        help='Folder name for downloads',
+        default='downloads',
     )
 
     parser.add_argument(
@@ -58,17 +78,49 @@ if __name__ == '__main__':
         for folder in data:
             for pres in folder['presentations']:
                 if pres['id'] in presentation_ids:
+                    # needed to get path etc
+                    pres['folder_path'] = folder['path']
+                    pres['folder_id'] = folder['id']
+                    pres['folder_catalogs'] = folder['catalogs']
                     presentations.append(pres)
     print(f'Found {len(presentations)} presentations')
     if presentations:
         for presentation in presentations:
-            print(f'Playing {presentation["id"]}')
+            pres_id = presentation["id"]
             video_urls = get_video_urls(presentation)
-            cmd = 'gst-launch-1.0'
-            for url in video_urls:
-                cmd += f' playbin uri={url}'
-            returncode = os.system(cmd)
-            if returncode != 0:
-                sys.exit()
+            if video_urls:
+                if not args.download:
+                    print(f'Playing {pres_id}')
+                    cmd = 'gst-launch-1.0'
+                    for url in video_urls.values():
+                        cmd += f' playbin uri={url}'
+                    returncode = os.system(cmd)
+                    if returncode != 0:
+                        sys.exit()
+                else:
+                    print(f'Downloading {pres_id}')
+                    root = Path(args.download_folder) / pres_id
+                    root.mkdir(parents=True, exist_ok=True)
+                    with requests.Session() as session:
+                        with open(root / 'mediasite_metadata.json', 'w') as f:
+                            json.dump(presentation, f, sort_keys=True, indent=4)
+                        for name, url in video_urls.items():
+                            local_filename = root / f'{name}.mp4'
+                            with session.get(url, stream=True) as r:
+                                r.raise_for_status()
+                                with open(local_filename, 'wb') as f:
+                                    print(f'Downloading {url} to {local_filename}')
+                                    downloaded = 0
+                                    chunk_size = 8192
+                                    for chunk in r.iter_content(chunk_size=chunk_size):
+                                        total_length = int(r.headers.get('content-length'))
+                                        downloaded += chunk_size
+                                        # If you have chunk encoded response uncomment if
+                                        # and set chunk_size parameter to None.
+                                        #if chunk:
+                                        print(f'{int(100 * downloaded/total_length)}%', end='\r')
+                                        f.write(chunk)
+            else:
+                print(f'No video urls found for {pres_id}')
     else:
         print(f'Presentation(s) {presentation_ids} not found')
