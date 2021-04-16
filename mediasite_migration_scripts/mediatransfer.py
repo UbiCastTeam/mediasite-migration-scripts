400614c1830da3ebbcc6ebd3b10d937978d1d77a
nantunes <nicolas.antunes@ubicast.eu>
2021-04-16 11:28:36 +0200

    fix inversed layout between webinar and video refs #33878

--- mediasite_migration_scripts/mediatransfer.py
+++ mediasite_migration_scripts/mediatransfer.py
@@ -299,7 +299,7 @@ class MediaTransfer():
                                 'transcode': 'yes' if v_type == 'audio_only' else 'no',
                                 'origin': 'mediatransfer',
                                 'detect_slides': 'yes' if v_type == 'computer_slides' or v_type == 'composite_slides' else 'no',
-                                'layout': 'webinar' if v_type == 'computer_slides' or v_type == 'audio_slides' else 'video',
+                                'layout': 'video' if v_type == 'computer_slides' or v_type == 'audio_slides' else 'webinar',
                                 'slides': presentation.get('slides'),
                                 'video_type': v_type,
                                 'file_url': v_url
