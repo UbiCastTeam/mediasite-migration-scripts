93f5713c8b1fecda331cdaeb0c1fb7dec45092b5
Florent Thiéry <florent.thiery@ubicast.eu>
2021-06-08 19:06:50 +0200

    batch_merge: add --max-duration option, display failed media and stats at the end

--- bin/batch_merge.py
+++ bin/batch_merge.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import time
 import argparse
 import logging
 import subprocess
@@ -12,15 +13,23 @@ class BatchMerge:
         path = Path(config["folder"])
         subfolders = [d for d in path.iterdir() if d.is_dir()]
         total = len(subfolders)
+        failed = list()
+        before = time.time()
         for index, sf in enumerate(subfolders):
             logging.info(utils.get_progress_string(index, total) + f' Merging {sf.name}')
-            self.merge(sf)
+            if not self.merge(sf):
+                failed.append(sf.name)
+        took_s = time.time() - before
+        took_per_media = utils.get_timecode_from_sec(took_s / total)
+        logging.info(f'Finished processing {total} media, took {utils.get_timecode_from_sec(took_s)} ({took_per_media} per media)')
+        if failed:
+            logging.error(f'{len(failed)} failed / {total}: {failed}')
 
     def merge(self, media_folder):
         logging.debug(f'Merging videos in folder : {media_folder}')
         layout_file = media_folder / 'mediaserver_layout.json'
         if not layout_file.is_file():
-            cmd = f'python3 bin/merge.py --width {self.config.get("composite_width", 1920)} --height {self.config.get("composite_height", 1080)} {media_folder}'
+            cmd = f'python3 bin/merge.py --width {self.config.get("composite_width", 1920)} --height {self.config.get("composite_height", 1080)} --max-duration={self.config["max_duration"]} {media_folder}'
             logging.debug(cmd)
             return_code, output = subprocess.getstatusoutput(cmd)
             if return_code != 0:
@@ -46,12 +55,19 @@ if __name__ == '__main__':
     parser.add_argument(
         'folder',
         type=str,
-        help='Folder name in which to look for media (single media)',
+        help='Folder name in which to look for media folders',
     )
 
     parser.add_argument(
         '--config-file',
         default='config.json',
+    )
+
+    parser.add_argument(
+        '--max-duration',
+        type=int,
+        help='Stop after this amount of seconds (disabled by default). Can be useful for looking at results quicker.',
+        default=0,
     )
 
     args = parser.parse_args()
